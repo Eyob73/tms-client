b63e2eb86dc22e425cfbc3ae63d9d02dae4988f8
Eyob <eyget73@gmail.com>
2026-08-13 14:15:32 +0300

    feat: implement authentication service and add credentials interceptor for HTTP requests

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-import { provideHttpClient, withXhr } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptors,
+  withXhr,
+  withXsrfConfiguration,
+} from '@angular/common/http';
 import { routes } from './app.routes';
+import { credentialsInterceptor } from './interceptors/credentials.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(
+      withInterceptors([credentialsInterceptor]),
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN', // Cookie name set by .NET server
+        headerName: 'X-XSRF-TOKEN', // Header expected by .NET server
+      }),
+    ),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimations(),
     provideRouter(routes, withComponentInputBinding()),
